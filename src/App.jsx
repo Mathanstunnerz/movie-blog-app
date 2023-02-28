12a5cf8860a2c3b4a7742269ad1d4bed87897cc9
Mathanstunnerz <mathanstunnerz95@gmail.com>
2023-02-28 16:32:41 +0530

    backend connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,162 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  Data()
+  Data();
 
   return (
     <div className="App">
-<Data />
-<Content />
+      <Data />
+      <Card />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
 function Data() {
-
-
-
-
-  return(
+  return (
     <div>
       <div className="main-body ">
         <div className="nav-bar">
-          
-          <img src="https://cdn.freebiesupply.com/logos/large/2x/movie-city-logo-png-transparent.png" className='logo-img'/>
-          <div className='theme-item'>
-          <a href='#'>Movie</a>
-          <a href='#'>Tv Show</a>
-          <a href='#'>Help</a>
+          <img
+            src="https://cdn.freebiesupply.com/logos/large/2x/movie-city-logo-png-transparent.png"
+            className="logo-img"
+          />
+          <div className="theme-item">
+            <a href="#">Movie</a>
+            <a href="#">Tv Show</a>
+            <a href="#">Help</a>
           </div>
           {/* <button onClick={clidd} >asuhfguayf</button> */}
-          <div className='search-nav' >
-            
-          <input type="text" className="input" placeholder="Enter movie name" />
-          <button className='search-button'>search</button>
-         
+          <div className="search-nav">
+            <input
+              type="text"
+              className="input"
+              placeholder="Enter movie name"
+            />
+            <button className="search-button">search</button>
           </div>
-          
-          
         </div>
       </div>
-
     </div>
-  )
+  );
 }
-function Content(){
 
-return(
-  <div className='...'>
-  <div className=" content-body ">
-<div className="container">
-<Card />
-</div>
-    
-  </div>
-  <div className='end-bar'>@Design_By_MathanStunnerz</div>
-  </div>
-)
-}
-function Card(){
-  const data = [
-    {
-      "name": "Vikram",
-      "poster": "https://m.media-amazon.com/images/M/MV5BMmJhYTYxMGEtNjQ5NS00MWZiLWEwN2ItYjJmMWE2YTU1YWYxXkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg",
-      "rating": 8.4,
-      "summary": "Members of a black ops team must track and eliminate a gang of masked murderers."
-    },
-    {
-      "name": "RRR",
-      "poster": "https://englishtribuneimages.blob.core.windows.net/gallary-content/2021/6/Desk/2021_6$largeimg_977224513.JPG",
-      "rating": 8.8,
-      "summary": "RRR is an upcoming Indian Telugu-language period action drama film directed by S. S. Rajamouli, and produced by D. V. V. Danayya of DVV Entertainments."
-    },
-    {
-      "name": "Iron man 2",
-      "poster": "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg",
-      "rating": 7,
-      "summary": "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy."
-    },
-    {
-      "name": "No Country for Old Men",
-      "poster": "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg",
-      "rating": 8.1,
-      "summary": "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money."
-    },
-    {
-      "name": "Jai Bhim",
-      "poster": "https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg",
-      "summary": "A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case",
-      "rating": 8.8
-    },
-    {
-      "name": "The Avengers",
-      "rating": 8,
-      "summary": "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
-      "poster": "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg"
-    },
-    {
-      "name": "Interstellar",
-      "poster": "https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg",
-      "rating": 8.6,
-      "summary": "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans."
-    },
-    {
-      "name": "Baahubali",
-      "poster": "https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg",
-      "rating": 8,
-      "summary": "In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy."
-    },
-    {
-      "name": "Ratatouille",
-      "poster": "https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=",
-      "rating": 8,
-      "summary": "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him."
-    },
-    {
-      "name": "PS2",
-      "poster": "https://m.media-amazon.com/images/M/MV5BYjFjMTQzY2EtZjQ5MC00NGUyLWJiYWMtZDI3MTQ1MGU4OGY2XkEyXkFqcGdeQXVyNDExMjcyMzA@._V1_.jpg",
-      "summary": "Ponniyin Selvan: I is an upcoming Indian Tamil-language epic period action film directed by Mani Ratnam, who co-wrote it with Elango Kumaravel and B. Jeyamohan",
-      "rating": 8
-    },
-    {
-      "name": "Thor: Ragnarok",
-      "poster": "https://m.media-amazon.com/images/M/MV5BMjMyNDkzMzI1OF5BMl5BanBnXkFtZTgwODcxODg5MjI@._V1_.jpg",
-      "summary": "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\\n of researchers, to find a new planet for humans.",
-      "rating": 8.8
-    }
-  ]
+function Card() {
+  const [movie, setmovie] = useState([]);
+
+  useEffect(() => {
+    fetch("https://data-base-six.vercel.app/movies")
+      .then((res) => res.json())
+      .then((data) => setmovie(data));
+  }, []);
+
   return (
-    <div>
-     
-        
-         {data.map(ele=>
-         
-          <div className="card-header">
-            
-            <div className='card-img'>
-         <img className='img' src={ele.poster}/>
-        
-         <div className='rating-align'>
-         <span className='rating material-symbols-outlined'>Star</span>
-         <span className='rating'>{ele.rating}</span>
-         
-         </div>
+    <div className="container__">
+      {movie.map((ele) => (
+        <div className="card-header" onClick={(e) => console.log(e)}>
+          <div className="card-img">
+            <img className="img" src={ele.poster} />
 
-         <span className='summary'>
-          <h2>{ele.name}</h2>
-          <span>{ele.summary}</span>
-          </span>
-         
-         </div>
-         </div>
-
-         )} 
-        
-      
+            <div className="rating-align">
+              <div className="starrating material-symbols-outlined">
+                Star
+                <span className="rating">{ele.rating}</span>
+              </div>
+              <div className="summary">
+                <h2>{ele.name}</h2>
+                <span>{ele.summary}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
